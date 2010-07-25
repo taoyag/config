b9bb60f5ac11fa8acba9a1423c6f49e261cfca64
taoyag <taoyag@gmail.com>
2010-07-25 22:07:46 +0900

    update

---
+++ vim/.vim/templates/template.scala
@@ -0,0 +1,2 @@
+class <+FILE NAME+> {
+}
---
+++ vim/.vim/templates/templateSpec.scala
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith
+
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class <+FILE NAME+> extends FlatSpec with ShouldMatchers {
+  <+CURSOR+>
+}
+
