dcd0258ded0ca07564e3d05a7a94d0d8fbf5e451
taoyag <taoyag@taoyagMacMini.local>
2017-08-13 12:18:22 +0900

    add hammerspoon config

---
+++ misc/hammerspoon/init.lua
@@ -0,0 +1,10 @@
+local function keyStroke(mods, key)
+  return function() hs.eventtap.keyStroke(mods, key, 0) end
+end
+
+local function remap(mods, key, fn)
+  return hs.hotkey.bind(mods, key, fn, nil, fn)
+end
+
+-- global
+remap({'ctrl'}, 'm', keyStroke({}, 'return'))
